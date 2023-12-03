50cc0d3fe197081ee30901c0243e2f6a37786adc
kimms21 <detriment@naver.com>
2023-12-03 22:41:37 +0900

    Changes

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from PIL import Image
 import os
 
 pjt_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/"
-img_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/images/"
-st.write(os.getcwd())
+# img_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/images/"
+img_path = "/mount/src/streamlit_test/images"
 
 main_image = Image.open(img_path + '단데기.png')
-
-
 # st.set_page_config(
 #     page_title = "아무튼 웹페이지임",
 #     page_icon = path + "단데기"
