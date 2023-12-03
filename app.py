74427144487ad5b46e4612b8431304ac66ce2e01
kimms21 <detriment@naver.com>
2023-12-03 22:15:02 +0900

    Changes

--- app.py
+++ app.py
@@ -16,10 +16,3 @@ st.title('아무튼 웹페이지임')
 st.image(main_image)
 st.sidebar.success("Select page")
 
-
-# col1, col2 = st.columns([1,1])
-#
-# with col1:
-#     st.link_button('남자',"https://www.youtube.com/watch?v=g8Deu7a9rS8")
-# with col2:
-#     st.button('여자')
