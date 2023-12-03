ac9fcf987082d64c921282a5dcf9c6e53739851b
kimms21 <detriment@naver.com>
2023-12-03 22:10:59 +0900

    Changes

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from PIL import Image
 
-view = [100,150,300]
-st.bar_chart(view)
+
+pjt_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/"
+img_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/images/"
+main_image = Image.open(img_path + '단데기.png')
+
+# st.set_page_config(
+#     page_title = "아무튼 웹페이지임",
+#     page_icon = path + "단데기"
+# )
+
+st.title('아무튼 웹페이지임')
+
+st.image(main_image)
+st.sidebar.success("Select page")
+
+
+# col1, col2 = st.columns([1,1])
+#
+# with col1:
+#     st.link_button('남자',"https://www.youtube.com/watch?v=g8Deu7a9rS8")
+# with col2:
+#     st.button('여자')
---
+++ pages/여자_profile.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import os
+
+target_sex = "여자"
+pjt_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/"
+img_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/images/"
+
+
+# st.set_page_config(
+#     page_title = "아무튼 웹페이지임",
+#     page_icon = ""
+# )
+
+def batch(iterable, n=1):
+   l = len(iterable)
+   for ndx in range(0, l, n):
+      yield iterable[ndx:min(ndx + n, l)]
+
+
+st.title(f'{target_sex} Profile')
+man_list = os.listdir(img_path+target_sex+"/")
+cols = st.columns(len(man_list))
+
+for idx, man_name in enumerate(man_list):
+
+   with cols[idx % 2]:
+      st.header(man_name.replace('.PNG', ''))
+      man_image = Image.open(img_path + target_sex + "/" + man_name)
+      st.image(man_image)
+      st.write(f"상태 : 진행중")
+      expander_stat = st.expander("profile")
+      with expander_stat:
+         st.write(f"키 : {np.random.randint(1,100)}")
+         st.write(f"몸무게 : {np.random.randint(1,100)}")
+      expander_introduce = st.expander("자기소개")
+      with expander_introduce:
+         expander_introduce.write("자기소개"+str(idx))
+      expander_opentalk = st.expander("오픈톡 링크")
+      with expander_opentalk:
+         expander_opentalk.write("opentalk_url" + str(idx))
+
+
+
+
+
