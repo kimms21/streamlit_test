d01760b843e0d3c84b69af33b1d151d76e6a95b4
kimms21 <detriment@naver.com>
2023-12-03 22:40:05 +0900

    Changes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from PIL import Image
-
+import os
 
 pjt_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/"
 img_path = "C:/Users/detri/OneDrive/바탕 화면/jupyter_home/streamlit_test/images/"
+st.write(os.getcwd())
+
 main_image = Image.open(img_path + '단데기.png')
+
 
 # st.set_page_config(
 #     page_title = "아무튼 웹페이지임",
