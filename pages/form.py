f1a0445c06af1020ac632339127c0ea99c511325
kimms21 <detriment@naver.com>
2023-12-16 22:55:30 +0900

    Changes

--- pages/form.py
+++ pages/form.py
@@ -22,13 +22,12 @@ tabs = st.tabs(tab_titles)
 with tabs[0]:
    with st.form("new"):
       is_error = False
-      st.write("Inside the form")
       name = st.text_input(label = "별명입력",value = "단데기")
       sex = st.selectbox(label = "성별", options=["남자","여자"])
       age = st.number_input(label = "나이",value = 25)
       height = st.number_input(label = "키(cm)",value = 180)
       mbti = st.text_input(label="MBTI",value = "ISTP")
-      introduce = st.text_area(label = "자기소개",value = "난단데기임")
+      introduce = st.text_area(label = "자기소개",value = "성격, 사는곳 등 자유롭게 적어주세요")
       openchat = st.text_input(label="오픈챗_url",value = "오픈챗주소")
       permission = st.text_input(label="관리자코드")
       uploaded_file = st.file_uploader("이미지는 256x256 size로 바뀌어 업로드됩니다")
@@ -52,7 +51,7 @@ with tabs[0]:
             st.rerun()
 
          else:
-            st.success("전송완료!")
+            st.success("신규요청완료! (profile 페이지에서 확인하세요)")
             bytes_data = uploaded_file.getvalue()
             uploaded_image = Image.open(uploaded_file)
             uploaded_image_reiszed = uploaded_image.resize((256, 256))
@@ -66,13 +65,13 @@ with tabs[1]:
       st.write("Inside the form")
       name_del = st.text_input(label = "별명입력",value = "단데기")
       submitted_del = st.form_submit_button("Submit")
-      if name_del in list(people_db["key"]):
-         db.delete(name_del)
-         drive_image.delete(f"{name_del}.PNG")
-
-      else:
-         st.error('사용자정보가 없습니다')
-         st.stop()
 
       if submitted_del:
-         st.success("전송완료!")
+         if name_del not in list(people_db["key"]):
+            st.error('사용자정보가 없습니다')
+            st.stop()
+
+         else:
+            db.delete(name_del)
+            drive_image.delete(f"{name_del}.PNG")
+            st.success("삭제요청완료!")
